3f39449778fa1c508ea1d6f8585333fbd7f0e33d
myhkas <mmyhkas@gmail.com>
2020-07-25 10:23:42 -0700

    added comment for clarity

--- controllers/mongo-controller.js
+++ controllers/mongo-controller.js
@@ -3,19 +3,22 @@ const databaseUrl = "notetaker_db";
 const collections = ["notes"];
 
 const db = mongojs(databaseUrl, collections);
-
+//check if there's connection issue
 db.on("error", (err) => console.log({ err_message: err }));
 
 module.exports = {
+  // returns object inside array even if empty or just one
   getNotes: (req, res) =>
     db.notes.find({}, (err, data) => (err ? res.send(err) : res.send(data))),
 
+  // getOne returns on object
   getNote: (req, res) => {
     db.notes.findOne({ _id: mongojs.ObjectID(req.params.id) }, (err, data) => {
       err ? res.send(err) : res.send(data);
     });
   },
 
+  //mongodb allows you to save tyour arrays and objects to the collections
   addNote: (req, res) => {
     db.notes.insert(
       {
@@ -27,6 +30,7 @@ module.exports = {
     );
   },
 
+  //anytime we a re referencing an id you must include mongojs.ObjectID
   updateText: (req, res) => {
     db.notes.update(
       { _id: mongojs.ObjectID(req.body.id) },
@@ -34,10 +38,10 @@ module.exports = {
       (err, response) => (err ? res.send(err) : res.send(response))
     );
   },
-
   addTag: (req, res) => {
     db.notes.update(
       { _id: mongojs.ObjectID(req.body.id) },
+      //to add single thing to array
       { $push: { tags: req.body.tags } },
       (err, response) => (err ? res.send(err) : res.send(response))
     );
@@ -46,6 +50,7 @@ module.exports = {
   addTags: (req, res) => {
     db.notes.update(
       { _id: mongojs.ObjectID(req.body.id) },
+      //to add multiple things into array
       { $push: { tags: { $each: req.body.tags } } },
       (err, response) => (err ? res.send(err) : res.send(response))
     );
@@ -54,6 +59,7 @@ module.exports = {
   removeTags: (req, res) => {
     db.notes.update(
       { _id: mongojs.ObjectID(req.body.id) },
+      //to pull a multiple items from a narray just use $pullAll
       {
         $pullAll: { tags: req.body.tags },
       },
@@ -67,6 +73,7 @@ module.exports = {
     db.notes.update(
       { _id: mongojs.ObjectID(req.body.id) },
       {
+        //to pull a single item from a narray just use $pull
         $pull: { tags: req.body.tags },
       },
       (err, data) => {
@@ -76,6 +83,8 @@ module.exports = {
   },
 
   deleteNote: (req, res) => {
+    //to remove an item from the collection by its id
+    //any parameter works
     db.notes.remove({ _id: mongojs.ObjectID(req.body.id) }, (err, data) =>
       err ? res.send(err) : res.send(data)
     );
